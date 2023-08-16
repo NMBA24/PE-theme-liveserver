106dac89d4665d55afa5c6507e8149ee87547983
Diane Kang <diane.kang@page-effect.com>
2023-08-16 15:08:00 +0200

    responsiv part1

--- index.js
+++ index.js
@@ -8,9 +8,20 @@ const swiper = new Swiper(".swiperCarousel", {
   direction: "horizontal",
   loop: true,
   //Amount of slides in loop mode should be at least 2x of slidesPerView value.
-  slidesPerView: "auto",
+  slidesPerView: 1.05,
   centeredSlides: true,
   spaceBetween: 24, // coloum gap 24px
+  breakpoints: {
+    // when window width is > 600px
+    601: {
+      slidesPerView: 1.3,
+      spaceBetweenSlides: 24,
+    },
+    901: {
+      slidesPerView: "auto",
+      spaceBetweenSlides: 24,
+    },
+  },
 
   keyboard: {
     enabled: true,
